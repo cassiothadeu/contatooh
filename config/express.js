d3bde79ee27a1310cd29e15d9eced6dbc25536c6
Cássio Thadeu <cassiothadeu@gmail.com>
2015-06-02 22:39:37 -0300

    Finalizando capitulo 9 tornando aplicação mais segura

--- config/express.js
+++ config/express.js
@@ -4,7 +4,8 @@ var express = require('express'),
 	cookieParser = require('cookie-parser'),
 	session = require('express-session'),
 	passport = require('passport'),
-  	methodOverride = require('method-override')()
+  	methodOverride = require('method-override')(),
+  	helmet = require('helmet')
 	;
 
 module.exports = function(){
@@ -30,11 +31,20 @@ module.exports = function(){
   app.use(passport.initialize());
   app.use(passport.session());
 
+  app.use(helmet.xframe());
+  app.use(helmet.xssFilter());
+  app.use(helmet.nosniff());
+  app.disable('x-powered-by');
 
   load('models', {cwd: 'app'})
-  	.then('controllers')
-  	.then('routes')
-  	.into(app);
+    .then('controllers')
+    .then('routes/auth.js')
+    .then('routes')
+    .into(app);
+
+  app.get('*', function(req, res) {
+     res.status(404).render('404');
+  });
 
   return app;
 };
